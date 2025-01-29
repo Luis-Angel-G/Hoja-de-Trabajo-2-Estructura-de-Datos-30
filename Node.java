ae45180fc976ba6d74bf6803f78297f547ed02b4
Luis-Angel-G <langelgirona@gmail.com>
2025-01-28 21:34:14 -0600

    Se agregan setters y getters

--- Node.java
+++ Node.java
@@ -2,4 +2,28 @@ public class Node<T> {
     Node<T> next = null;
     Node<T> previous = null;
     T value;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node<T> previous) {
+        this.previous = previous;
+    }
+
+    public T getValue() {
+        return value;
+    }
 }
