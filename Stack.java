95398d99012ddb9de32256ea06e01f36aa486294
MagicBag-tab <est24347@uvg.edu.gt>
2025-01-29 20:24:20 -0600

    Version actual 29, 20:23 de la clase Stack sin modificar en esta branch

--- Stack.java
+++ Stack.java
@@ -1,37 +1,58 @@
 public class Stack<T> implements IStack<T> {
-    Node<T> first;
-    Node<T> last;
+    Node<T> first = new Node<>(null);
+    Node<T> last = first;
 
     @Override
     public void push(T value) {
-        last.setNext(new Node<T>(value));
+        last.setNext(new Node<>(value));
+        Node<T> aux = last;
         last = last.getNext();
-        last.setPrevious(last);
+        last.setPrevious(aux);
     }
 
     @Override
     public T pop() {
+        if (last == null) {
+            throw new IllegalStateException("La pila esta vacia");
+        }
         T value = last.getValue();
         last = last.getPrevious();
-        last.setNext(null);
+        if (last != null) {
+            last.setNext(null);
+        } else {
+            first = null;
+        }
         return value;
     }
 
     @Override
+    @SuppressWarnings({ "unchecked", "UnnecessaryBoxing" })
     public T operation(char operator, T value1, T value2) {
-        switch(operator) {
+        int result = 0;
+        int v1 = ((Integer) value1);
+        int v2 = ((Integer) value2);
+        switch (operator) {
             case '+':
-                return value1 + value2;
+                result = v1 + v2;
+                break;
             case '-':
-                return value1 - value2;
+                result = v1 - v2;
+                break;
             case '*':
-                return value1 * value2;
+                result = v1 * v2;
+                break;
             case '/':
-                return value1 / value2;
+                if (v2 == 0) {
+                    throw new ArithmeticException("División por 0");
+                }
+                result = v1 / v2;
+                break;
             case '%':
-                return value1 % value2;
+                result = v1 % v2;
+                break;
             default:
-                return 0;
+                throw new IllegalArgumentException("Operador no valido");
         }
+        return (T) Integer.valueOf(result);
     }
 }
