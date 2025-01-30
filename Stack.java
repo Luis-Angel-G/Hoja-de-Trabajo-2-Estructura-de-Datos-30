71ca6abe4ff94e4111968b4b4580057dfa795dd1
Luis-Angel-G <langelgirona@gmail.com>
2025-01-29 19:33:34 -0600

    Se corrigen metodo pop y push

--- Stack.java
+++ Stack.java
@@ -1,37 +1,63 @@
 public class Stack<T> implements IStack<T> {
-    Node<T> first;
-    Node<T> last;
+    Node<T> first = null;
+    Node<T> last = null;
 
     @Override
     public void push(T value) {
-        last.setNext(new Node<T>(value));
-        last = last.getNext();
-        last.setPrevious(last);
+        if (last == null) {
+            first = new Node<>(value);
+            last = first;
+        } else {
+            last.setNext(new Node<>(value));
+            Node<T> aux = last;
+            last = last.getNext();
+            last.setPrevious(aux);
+        }
     }
 
     @Override
     public T pop() {
+        if (last == null) {
+            throw new IllegalStateException("La pila esta vacia");
+        }
         T value = last.getValue();
         last = last.getPrevious();
-        last.setNext(null);
+        if (last != null) {
+            last.setNext(null);
+        } else {
+            first = null;
+        }
         return value;
     }
 
     @Override
+    @SuppressWarnings({"unchecked", "UnnecessaryBoxing"})
     public T operation(char operator, T value1, T value2) {
-        switch(operator) {
+        int result = 0;
+        int v1 = ((Integer) value1);
+        int v2 = ((Integer) value2);
+        switch (operator) {
             case '+':
-                return value1 + value2;
+                result = v1 + v2;
+                break;
             case '-':
-                return value1 - value2;
+                result = v1 - v2;
+                break;
             case '*':
-                return value1 * value2;
+                result = v1 * v2;
+                break;
             case '/':
-                return value1 / value2;
+                if (v2 == 0) {
+                    throw new ArithmeticException("División por 0");
+                }
+                result = v1 / v2;
+                break;
             case '%':
-                return value1 % value2;
+                result = v1 % v2;
+                break;
             default:
-                return 0;
+                throw new IllegalArgumentException("Operador no valido");
         }
+        return (T) Integer.valueOf(result);
     }
 }
