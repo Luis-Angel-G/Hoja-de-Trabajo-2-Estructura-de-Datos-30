a7a37dfef37d3f7a1d638f9993164634e9a31adc
Luis-Angel-G <langelgirona@gmail.com>
2025-01-30 22:20:42 -0600

    Se crea Javadoc

--- Stack.java
+++ Stack.java
@@ -1,7 +1,30 @@
+/**
+ * La clase Stack implementa la interfaz IStack.
+ * @Project : Hoja de trabajo 2
+ * @author Luis Girón
+ * Creacion 28.01.2025
+ * Ultima modificacion 30.01.2025
+ * @File Name: Stack.java
+ */
+
+/**
+ * La clase Stack implementa la interfaz IStack y proporciona la funcionalidad de una pila.
+ *
+ * @param <T> el tipo de elementos en la pila
+ */
 public class Stack<T> implements IStack<T> {
+
+    /** Creación del primer valor de la pila como nulo. */
     Node<T> first = new Node<>(null);
+
+    /** Creación del último valor de la pila como el primero. */
     Node<T> last = first;
 
+    /**
+     * Agrega un valor a la pila.
+     *
+     * @param value el valor a ser agregado a la pila
+     */
     @Override
     public void push(T value) {
         last.setNext(new Node<>(value));
@@ -10,6 +33,12 @@ public class Stack<T> implements IStack<T> {
         last.setPrevious(aux);
     }
 
+    /**
+     * Elimina y devuelve el valor en la parte superior de la pila.
+     *
+     * @return el valor en la parte superior de la pila
+     * @throws IllegalStateException si la pila está vacía
+     */
     @Override
     public T pop() {
         if (last == null) {
@@ -25,6 +54,14 @@ public class Stack<T> implements IStack<T> {
         return value;
     }
 
+    /**
+     * Realiza una operación con dos valores utilizando el operador dado.
+     *
+     * @param operator el operador a ser utilizado en la operación
+     * @param value1 el primer valor para la operación
+     * @param value2 el segundo valor para la operación
+     * @return el resultado de la operación
+     */
     @Override
     @SuppressWarnings({"unchecked", "UnnecessaryBoxing"})
     public T operation(char operator, T value1, T value2) {
@@ -42,16 +79,14 @@ public class Stack<T> implements IStack<T> {
                 result = v1 * v2;
                 break;
             case '/':
-                if (v2 == 0) {
-                    throw new ArithmeticException("División por 0");
+                if (v2 != 0) {
+                    result = v1 / v2;
+                } else {
+                    throw new ArithmeticException("Division por cero");
                 }
-                result = v1 / v2;
-                break;
-            case '%':
-                result = v1 % v2;
                 break;
             default:
-                throw new IllegalArgumentException("Operador no valido");
+                throw new IllegalArgumentException("Operador no soportado");
         }
         return (T) Integer.valueOf(result);
     }
